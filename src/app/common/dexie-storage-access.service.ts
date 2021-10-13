ccb6aae2bb6cfe2dd845570903168e1983cdb01e
Keerti V Kotaru <kvkirthy@gmail.com>
2021-10-13 13:41:22 +0530

    Minor updates to Dexie service.

--- src/app/common/dexie-storage-access.service.ts
+++ src/app/common/dexie-storage-access.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
-import { WebArcadeDB } from './web-arcade-db';
-import { CommentsEntity } from './comments-entity';
+import { WebArcadeDb } from 'src/app/common/web-arcade-db';
+import { CommentsEntity } from 'src/app/common/comments-entity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DexieStorageAccessService {
 
-  webArcadeDb = new WebArcadeDB();
+  webArcadeDb = new WebArcadeDb();
 
-  constructor(private windowObj: Window) {
-
-  }
+  constructor(private windowObj: Window) {}
 
   init(){
     this.webArcadeDb
@@ -19,27 +17,41 @@ export class DexieStorageAccessService {
     .catch(err => console.log("Dexie, error opening DB"));
 
     this.windowObj.addEventListener("online", (event) => {
-      this.getAllCachedComments();
+      // this.getAllCachedComments();
     });
+
+    this.updateComment(1, "New title", "new comment description");
   }
 
   addComment(title: string, userName: string, comments: string, gameId: number, timeCommented = new Date()){
-    this.webArcadeDb
-      .comments
-      .add({
-        title,
-        userName,
-        timeCommented: `${timeCommented.getMonth()}/${timeCommented.getDate()}/${timeCommented.getFullYear()}`,
-        comments,
-        gameId,
-      });
+      this.webArcadeDb
+        .comments
+        .add({
+          title,
+          userName,
+          timeCommented: `${timeCommented.getMonth()}/${timeCommented.getDate()}/${timeCommented.getFullYear()}`,
+          comments,
+          gameId,
+        })
+        .then( id => console.log(`Comment added successfully. Comment Id is ${id}`))
+        .catch( error => console.log(error))
+        ;
   }
   
   deleteComment(id: number){
-      this.webArcadeDb
+      return this.webArcadeDb
         .comments
         .delete(id)
-        .catch(err => console.error("delete error", err));
+        .then( () => console.log(`Comment deleted successfully.`))
+        .catch(err => console.error("Error deleting", err));
+  }
+
+  updateComment(commentId: number, title: string, comments: string){
+    this.webArcadeDb 
+      .comments
+      .update(commentId, {title: title, comments})
+      .then( result => console.log(`Comment updated successfully. Updated record ID is ${result}`))
+      .catch(error => console.error("Error updating", error));
   }
 
   getAllCachedComments(){
@@ -47,11 +59,11 @@ export class DexieStorageAccessService {
     this.webArcadeDb.transaction("rw", this.webArcadeDb.comments, () => {
 
     this.webArcadeDb
-      .comments
+      .comments      
       .each( (entity: CommentsEntity) => {
-        console.log("each", entity);
-        this.deleteComment(entity.IdxCommentId || 0);
-      });
+        console.log(entity);
+      })
+      .catch(error => console.error("Error updating", error));
     });
   }
 }
--- src/app/common/web-arcade-db.ts
+++ src/app/common/web-arcade-db.ts
@@ -4,14 +4,21 @@ import { CommentsEntity } from './comments-entity';
 const WEB_ARCADE_DB_NAME = 'web-arcade-dexie';
 const OBJECT_STORE_GAME_COMMENTS = 'gameComments';
 
-export class WebArcadeDB extends Dexie {
+export class WebArcadeDb extends Dexie {
     comments: Dexie.Table<CommentsEntity>;
     
     constructor() {
       super(WEB_ARCADE_DB_NAME);
-      this.version(2).stores({
-        gameComments: '++IdxCommentId,timeCommented, userName'
-      });
+      this.version(1.2).stores({
+        gameComments: '++IdxCommentId,timeCommented, userName, gameId',
+        boardGameComments: null 
+      }).upgrade( idb => 
+        idb.table(OBJECT_STORE_GAME_COMMENTS)
+          .toCollection()
+          .modify( comments => {
+            delete comments.userName;
+          }) );
+     
       this.comments = this.table(OBJECT_STORE_GAME_COMMENTS);
      }
   }
